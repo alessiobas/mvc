e316de68082d870d5d818b2886bd103a78cb4ce7
Alessio Basile <alessio.basile.jr@gmail.com>
2022-04-15 11:25:04 +0200

    Controller for Card and Deck

---
+++ src/Controller/CardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CardController extends AbstractController
+{
+    /**
+     * @Route("/card", name="card-home")
+     */
+    public function card(): Response
+    {
+        return $this->render('card/card-home.html.twig', []);
+    }
+}
