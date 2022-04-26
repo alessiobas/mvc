2fcb37b24ec036d783c5a749bb1cd0c571090a68
Alessio Basile <alessio.basile.jr@gmail.com>
2022-04-26 09:29:57 +0200

    Added PHP Linters and Mess detection tools

---
+++ tools/phpstan/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fc526d9907143d1b57f84e7aadb1ac7fbee8f228',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fc526d9907143d1b57f84e7aadb1ac7fbee8f228',
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.6.0',
+            'version' => '1.6.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'reference' => 'b480ba2ae0699a72d43a340c20b9c00ede91ee3e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
