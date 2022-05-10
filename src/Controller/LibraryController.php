d3a84033fc6c089a19933ab4f7ebb913e986e69d
Alessio Basile <alessio.basile.jr@gmail.com>
2022-05-10 16:20:58 +0200

    Create routes for create, update, delete

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\LibraryRepository;
+use App\Entity\Library;
+use Doctrine\Persistence\ManagerRegistry;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -33,5 +35,132 @@ class LibraryController extends AbstractController
 
         return $this->render('library/show-books.html.twig', $data);
     }
+
+    /**
+     * @Route("/library/show/{id}", name="book_id")
+     */
+    public function showBook(
+        LibraryRepository $libraryRepository,
+        int $id
+    ): Response {
+        $book = $libraryRepository
+            ->find($id);
+            $data = [
+                "book" => $book
+            ];
+            return $this->render('library/show-book.html.twig', $data);
+    }
+
+    /**
+     * @Route("/library/create", name="create_book", methods={"GET", "HEAD"})
+     */
+    public function createBook(): Response
+    {
+        return $this->render('library/create-book-form.html.twig', []);
+    }
+
+    /**
+     * @Route("/library/create", name="create_book_process", methods={"POST"})
+     */
+    public function createBookSession(
+        Request $request,
+        ManagerRegistry $doctrine
+    ): Response {
+        $entityManager = $doctrine->getManager();
+
+        $title = $request->request->get("title");
+        $author = $request->request->get("author");
+        $isbn = $request->request->get("isbn");
+        $imgUrl = $request->request->get("imageUrl");
+
+        $books = new Library();
+        $books->setTitle($title);
+        $books->setAuthor($author);
+        $books->setIsbn($isbn);
+        $books->setImage($imgUrl);
+
+        // tell Doctrine you want to (eventually) save the Product
+        // (no queries yet)
+        $entityManager->persist($books);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library_show_all');
+    }
+
+    /**
+     * @Route("/library/update/{id}", name="update_book_id", methods={"GET", "HEAD"})
+     */
+    public function updateBook(
+        LibraryRepository $libraryRepository,
+        int $id
+    ): Response {
+        $book = $libraryRepository
+            ->find($id);
+
+            $data = [
+                "book" => $book
+            ];
+
+            return $this->render('library/update-book.html.twig', $data);
+    }
+
+    /**
+     * @Route("/library/update/{id}", name="update_book_id_process", methods={"POST"})
+     */
+    public function updateBookSession(
+        Request $request,
+        ManagerRegistry $doctrine,
+        LibraryRepository $libraryRepository
+    ): Response {
+        $entityManager = $doctrine->getManager();
+
+        $id = $request->request->get("id");
+        $title = $request->request->get("title");
+        $author = $request->request->get("author");
+        $isbn = $request->request->get("isbn");
+        $imgUrl = $request->request->get("imageUrl");
+
+        $books = $libraryRepository
+            ->find($id);
+        ;
+        $books->setTitle($title);
+        $books->setAuthor($author);
+        $books->setIsbn($isbn);
+        $books->setImage($imgUrl);
+
+        // tell Doctrine you want to (eventually) save the Product
+        // (no queries yet)
+        $entityManager->persist($books);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library_show_all');
+    }
+
+    /**
+     * @Route("/library/delete/{id}", name="delete_book_id")
+     */
+    public function deleteBook(
+        ManagerRegistry $doctrine,
+        int $id
+    ): Response {
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Library::class)->find($id);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No book found for id '.$id
+            );
+        }
+
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library_show_all');
+    }
+
 
 }
