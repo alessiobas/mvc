c8108081e10b4a3757a90ab04ea765e48e70bc69
Alessio Basile <alessio.basile.jr@gmail.com>
2022-05-10 15:05:27 +0200

    Created tables and fields for books, controller for library and library class

---
+++ src/Controller/LibraryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\LibraryRepository;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LibraryController extends AbstractController
+{
+    #[Route('/library', name: 'app_library')]
+    public function index(): Response
+    {
+        return $this->render('library/index.html.twig', [
+            'controller_name' => 'LibraryController',
+        ]);
+    }
+
+    /**
+    * @Route("/library/show", name="library_show_all")
+    */
+    public function showAllBooks(
+        LibraryRepository $libraryRepository
+    ): Response {
+        $books = $libraryRepository
+            ->findAll();
+
+        return $this->json($products);
+    }
+
+}
---
+++ src/Entity/Library.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LibraryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LibraryRepository::class)]
+class Library
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $titel;
+
+    #[ORM\Column(type: 'integer')]
+    private $isbn;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $author;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitel(): ?string
+    {
+        return $this->titel;
+    }
+
+    public function setTitel(?string $titel): self
+    {
+        $this->titel = $titel;
+
+        return $this;
+    }
+
+    public function getIsbn(): ?int
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(int $isbn): self
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
