027e1f28a91140523235ab45144bb7fa76f6a5e3
Alessio Basile <alessio.basile.jr@gmail.com>
2022-05-17 20:38:02 +0200

    Base of exam page updated

--- src/Controller/ExamController.php
+++ src/Controller/ExamController.php
@@ -8,14 +8,50 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class ExamController extends AbstractController
 {
     /**
      * @Route("/proj", name="proj-home")
      */
-    public function index(): Response
+    public function index(ChartBuilderInterface $chartBuilder): Response
     {
-        return $this->render('exam/index.html.twig');
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([
+            'scales' => [
+                'y' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => 100,
+                ],
+            ],
+        ]);
+
+        return $this->render('exam/index.html.twig', [
+            'chart' => $chart,
+        ]);
+    }
+
+
+    /**
+     * @Route("/proj/about", name="proj-about")
+     */
+    public function about(): Response
+    {
+        return $this->render('exam/about.html.twig');
     }
 }
